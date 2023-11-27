90642a5f70fca65019108ce9b462cacffb3c1fcb
ecrvmal <ecrvmal@yandex.ru>
2023-11-27 22:58:56 +0300

    Sprint2 completed

--- main.py
+++ main.py
@@ -11,5 +11,38 @@ fake_users = [
 ]
 
 @app.get("/users/{user_id}")
-def get_user(user_id):
-    return user_id
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
+
+# request : http://127.0.0.1:8000/users/2
+# [{"id":2,"role":"investor","name":"John"}]
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+@app.get("/trades")
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+# http://127.0.0.1:8000/trades?limit=1
+# [{"id":1,"user_id":1,"currency":"BTC","side":"buy","price":123,"amount":2.12}]
+# http://127.0.0.1:8000/trades?limit=1&offset=1
+# [{"id":2,"user_id":1,"currency":"BTC","side":"sell","price":125,"amount":2.12}]
+
+
+
+fake_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
