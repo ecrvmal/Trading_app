ff203e9d21f77e09ebfcf09d13574b87728e0bd8
ecrvmal <ecrvmal@yandex.ru>
2023-11-29 15:44:05 +0300

    sprint4 part1

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from datetime import datetime
+from typing import List, Optional
+
+from fastapi import FastAPI, Request, status
+from pydantic import BaseModel, Field
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI(
     title="Trading App"
 )
+
+
+# Благодаря этой функции клиент видит ошибки, происходящие на сервере,
+# вместо "Internal server error"
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),          # client will see error detail
+    )
+
+
 
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
@@ -10,12 +30,30 @@ fake_users = [
     {"id": 3, "role": "trader", "name": "Matt"},
 ]
 
-@app.get("/users/{user_id}")
+class DegreeType(BaseModel):
+    newbie = "newbie"
+    expert = "expert"
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType     #  newbie | expert  if else > error
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: list[Degree]
+
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
 # request : http://127.0.0.1:8000/users/2
 # [{"id":2,"role":"investor","name":"John"}]
+
 
 fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
@@ -41,8 +79,28 @@ fake_users2 = [
 ]
 
 
+
 @app.post("/users/{user_id}")
 def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+
+# to describe Trade we use data model (Pydentic)
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: int
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):   # input : list of deals
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
+
+
---
+++ models/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import MetaData, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON)
+)
+
+rusers = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+
+)
