7626d752e566e6b8d5112564c4f1890e6ac31643
ecrvmal <ecrvmal@yandex.ru>
2023-12-01 19:58:38 +0300

    Sprint4 done

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import MetaData, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from datetime import datetime
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
 
 metadata = MetaData()
 
@@ -10,11 +11,13 @@ roles = Table(
     Column("permissions", JSON)
 )
 
-rusers = Table(
+users = Table(
     "users",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey("roles.id")),
 )
