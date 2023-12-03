9ba761f215ff78c60a06ec0f3d99764037be86a0
Vladimir Malchevskiy <102606951+ecrvmal@users.noreply.github.com>
2023-12-03 19:36:30 +0300

    Sprint3 (#3)
    * Sprint3

--- main.py
+++ main.py
@@ -46,4 +46,3 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
-
