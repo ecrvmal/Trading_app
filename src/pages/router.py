a6db83aa21d7b69c8713c65918eb402e05473ffc
ecrvmal <ecrvmal@yandex.ru>
2023-12-22 00:54:10 +0300

    webockets_via_java_script done

--- src/pages/router.py
+++ src/pages/router.py
@@ -25,3 +25,7 @@ def get_search_page(request: Request, operations=Depends(get_specific_operations
             # далее выполнится operations/get_specific_operation, данные operations подставятся в шаблонизатор
     return templates.TemplateResponse("search.html", {"request": request, "operations": operations["data"]})
 
+@router.get("/chat")
+def get_chat_page(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
